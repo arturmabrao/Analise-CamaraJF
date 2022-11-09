d0299f15eb3d24d830657e86368c8ca31e824ab6
arturmabrao <arturmabrao@gmail.com>
2022-11-09 15:36:30 -0300

    Organizacao

--- Database/SQL/CreateInsert/inserts_iniciais.sql
+++ Database/SQL/CreateInsert/inserts_iniciais.sql
@@ -18,13 +18,13 @@ UPDATE atividade_camarajf.autoria
 					WHERE atividade_camarajf.projeto.id_projeto_camara = atividade_camarajf.autoria.id_projeto_camara)
  WHERE id_projeto_camara = (SELECT atividade_camarajf.projeto.id_projeto_camara 
 							FROM atividade_camarajf.projeto
-							WHERE atividade_camarajf.projeto.id_projeto_camara = atividade_camarajf.autoria.id_projeto_camara)
+							WHERE atividade_camarajf.projeto.id_projeto_camara = atividade_camarajf.autoria.id_projeto_camara);
 							
 
 -- INSERT TABELA PARLAMENTAR
 INSERT INTO atividade_camarajf.parlamentar  (nome_camara)
 	SELECT DISTINCT(nome_autor) 
-	FROM atividade_camarajf.autoria
+	FROM atividade_camarajf.autoria;
 	
 
 -- AUTALIZACAO DA TABELA DE AUTORIA, ATRIBUINDO OS IDS DOS PARLAMENTARES AO CAMPO FK
@@ -34,6 +34,4 @@ UPDATE atividade_camarajf.autoria
 					WHERE atividade_camarajf.parlamentar.nome_camara = atividade_camarajf.autoria.nome_autor)
  WHERE nome_autor = (SELECT atividade_camarajf.parlamentar.nome_camara 
 					FROM atividade_camarajf.parlamentar
-					WHERE atividade_camarajf.parlamentar.nome_camara = atividade_camarajf.autoria.nome_autor)
-
-
+					WHERE atividade_camarajf.parlamentar.nome_camara = atividade_camarajf.autoria.nome_autor);
