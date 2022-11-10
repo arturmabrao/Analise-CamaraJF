310269fa731418d954bd920d211ee5ef3d61fcf0
arturmabrao <arturmabrao@gmail.com>
2022-11-10 11:09:59 -0300

    SQL Autores

---
+++ Database/SQL/Queries/autores_projetos.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT(aut.nome_autor)
+
+FROM atividade_camarajf_simp.atividade_camarajf_norm.autoria		aut
+
+--WHERE  lower(aut.nome_autor) like '%fig%';
---
+++ Database/SQL/Queries/normalizacao_nomesautores.sql
@@ -0,0 +1,25 @@
+UPDATE atividade_camarajf_simp.atividade_camarajf_norm.autoria
+    SET nome_autor = 'Figueirôa'
+    WHERE nome_autor IN ('Figueiôa'
+                        ,'José Sóter de Figueirôa Neto'
+                        ,'Figueriôa'
+                        ,'Figuerôa'
+                        ,'Figueiroa'
+                        ,'Figueirôa'
+                        , 'Foigueirôa');
+
+UPDATE atividade_camarajf_simp.atividade_camarajf_norm.autoria
+    SET nome_autor = 'Flávio Cheker'
+    WHERE nome_autor IN ('Vereador Flávio Cheker'
+                        ,'Flavio Cheker'
+                        ,'Flávio Cheker'
+                        ,'FlávioCheker'
+                        ,'Flávio Procópio Cheker');
+
+UPDATE atividade_camarajf_simp.atividade_camarajf_norm.autoria
+    SET nome_autor = 'Fiorilo'
+    WHERE nome_autor IN ('José Mansueto Fiorilo'
+                        ,'Dr Fiorilo'
+                        ,'Fiorilo');
+
+
